e7e93748412cacc8c78f84cc9ddc537186e9ff25
Delmar Filho <ddlmar13@gmail.com>
2024-09-01 12:47:14 -0300

    feat :sparkles: added create module

--- src/commands/generate-module.js
+++ src/commands/generate-module.js
@@ -4,12 +4,24 @@ const command = {
   run: async (toolbox) => {
     const { print, parameters, template, filesystem } = toolbox
 
+    const basePath = 'src/modules'
+
     if (!parameters.first) {
       print.error('Please provide a module name')
       return
     }
 
-    filesystem.dir('src/modules/aa')
+    if (filesystem.exists(`src/modules/${parameters.first}`)) {
+      print.error('Module already exists')
+      return
+    }
+
+    filesystem.dir(`${basePath}/${parameters.first}/controllers`)
+    filesystem.dir(`${basePath}/${parameters.first}/views`)
+    filesystem.dir(`${basePath}/${parameters.first}/hooks`)
+    filesystem.dir(`${basePath}/${parameters.first}/utils`)
+    filesystem.dir(`${basePath}/${parameters.first}/components`)
+    filesystem.dir(`${basePath}/${parameters.first}/contexts`)
 
     // await template.generate({
     //   template: '',
